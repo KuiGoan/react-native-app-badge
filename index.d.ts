fd9c5b3cde134854c90184fa6fc36f2bab09678f
quydd <quydangdac@gmail.com>
2022-10-25 14:51:51 +0700

    update interface

--- index.d.ts
+++ index.d.ts
@@ -2,29 +2,9 @@ declare module "react-native-app-badge" {
 
     interface ShortcutBadgeStatic {
         /**
-         * Launcher home name, null if unknown
-         */
-        readonly launcher: string | null;
-
-        /**
-         * If the launcher supported?
-         */
-        readonly supported: boolean;
-
-        /**
          * Set the badge count. Use 0 to remove it.
          */
-        setCount(count: number): Promise<boolean>;
-
-        /**
-         * Get the badge count.
-         */
-        getCount(): Promise<number>;
-
-        /**
-         * Reads the counter from the storage
-         */
-        requestPermission(): Promise<void>;
+        setCount(count: number);
     }
 
     const ShortcutBadge: ShortcutBadgeStatic;
